9a649799436c6da8293460f5e722c8aa5db09cdd
Huust <git@github.com:Huust/Rie_OS.git>
2020-04-08 16:29:48 +0800

    second commit

--- loader.asm
+++ loader.asm
@@ -59,3 +59,37 @@ mov cr0,eax
 
 ;涉及loader开启保护模式后显示部分的内容
 ;主要就是利用显卡打印一些字符通知已经进入保护模式了
+
+
+
+
+;------------------为页表的开启做准备-----------------------
+;流程：创建页目录表和页表；cr3寄存器存放页目录表基址；cr0寄存器pg位置位，开启页表
+;最高1G的size分配给内核，低3G分配给用户
+;最后一个目录项指向PDT本身的起始地址
+set_up_page vstart = 0x100000:
+;allocate mem
+dd (0x101000 || 111B)
+times 766 dd 0x00
+dd (0x101000 || 111B)
+times 256 dd 0x00
+dd (0x00 || 111B)
+times 1023 dd 0x00
+;cr3 load address
+mov eax,cr3
+or eax,set_up_page
+mov cr3,eax
+;reload gdt
+sgdt [gdt_ptr]
+mov ebx, [gdt_ptr + 2]
+or dword [ebx + 0x18 + 4], 0xc0000000
+add dword [gdt_ptr + 2], 0xc0000000
+add esp, 0xc0000000
+;set pg in cr0,paging begin
+mov eax,cr0
+or eax,(1B<<31)
+mov cr0,eax
+lgdt [gdt_ptr]  ;here to update gdt address
+
+
+
