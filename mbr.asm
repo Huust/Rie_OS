08c65739846b35fd9402fac6b3f3f6d2ddf54ec6
Huust <git@github.com:Huust/Rie_OS.git>
2020-04-16 22:30:28 +0800

    consummate  mbr.asm

--- mbr.asm
+++ mbr.asm
@@ -1,11 +1,12 @@
 ;mbr主引导程序
 ;update date:2020-03-30
-LOADER_IN_MEM equ 0x9000
-LOADER_START_SECT equ 0x02
+;second update:2020-04-16
+LOADER_IN_MEM equ 0x9000    ;loader.asm will be loaded from 0x80000
+LOADER_START_SECTOR equ 0x02  ;loader.asm in disk
 
-section mbr vstart=0x7c00
-;段寄存器初始化为0--习惯性操作
-mov ax,cs
+
+section mbr vstart=0x7c00:
+mov ax,cs   ;the moment cs==0
 mov ss,ax
 mov es,ax
 mov ds,ax
@@ -16,21 +17,35 @@ mov es,ax
 ;- why sp is given the value 0x7c00
 ;- because stack goes downward and code goes upward
 mov sp,0x7c00 
+
 ;bios int 0x10 to clear the screen
-mov ah,0x06     ;ah是功能编码，0x06向上滚屏；之前忽略了这个参数
-mov al,0x00
-mov ch,0x03
-mov cl,0x03
-mov dh,0x4b 
-mov dl,0x14
-mov bh,0x17
+mov ah,0x06 ;ah是功能编码，0x06向上滚屏；之前忽略了这个参数
+mov al,0   
+mov ch,1   
+mov cl,1   
+mov dh,24  
+mov dl,84  
+mov bh,0x07
 int 0x10
 
-;成功调用call后loader被写入内存；之后jmp到loader所在位置开始执行
-call loader_load_in
-jmp LOADER_START_SECT
+mov byte [es:0],'R'
+mov byte [es:1],0x07
+mov byte [es:2],'i'
+mov byte [es:3],0x07
+mov byte [es:4],'e'
+mov byte [es:5],0x07
+mov byte [es:6],'O'
+mov byte [es:7],0x07
+mov byte [es:8],'S'
+mov byte [es:9],0x07
 
-loader_load_in:
+;-----------------显示部分结束，开始处理loader--------------
+
+;成功调用call后loader被写入内存；之后jmp到loader所在位置开始执行
+call loadin_loader
+jmp LOADER_IN_MEM
+
+loadin_loader:
 ;0x1f2:number of disk sections to write   0x1f3-0x1f5:LBA0-23   
 ;0x1f6:LBA24-27   0x1f7:command&status register
 mov dx,0x1f2
@@ -57,11 +72,11 @@ mov dx,0x1f7
 mov al,0x20
 out dx,al
 
-;采用轮询方法等待状态寄存器3,7位满足要求
+;采用轮询方法等待状态寄存器(0x17f端口)3,7位满足要求
 .QuestForRead:
 in al,dx
 mov bl,0x88 
-and bl,ax
+and bl,al
 cmp bl,0x08
 jnz .QuestForRead
 
@@ -71,7 +86,7 @@ mov cx,256
 
 ;将读取的数据写入内存
 .WriteToMem:
-in ax,dx    ;注意，在读取16位数据时，应该是每in指令是，自动向后获取新的16bit数据
+in ax,dx    ;注意，在读取16位数据时，应该是每次in指令，自动向后获取新的16bit数据
 mov [bx],ax
 add bx,2
 loop .WriteToMem
