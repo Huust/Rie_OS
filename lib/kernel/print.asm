b66db590ed01351a703664be5a485c82ea79dfc0
Huust <git@github.com:Huust/Rie_OS.git>
2020-04-26 22:38:13 +0800

    fix bug

--- lib/kernel/print.asm
+++ lib/kernel/print.asm
@@ -10,11 +10,15 @@ VIDEO_INDEX equ 0x0003
 VIDEO_SELECTOR equ (VIDEO_INDEX<<3|VIDEO_TI|VIDEO_RPI)
 ;create the selector and give to gs
 mov ax,VIDEO_SELECTOR
-mov gs,ax   ;这里赋值的原因请page287
-rie_putchar:
-;push all to enter the function
-pushad
+mov gs,ax   ;这里的赋值考虑到以后用户态调用
+
+[bits 32]
+
+global rie_putc
+rie_putc:
+pushad  ;push all register
 ;locate the cursor
+;high 8-bit
 mov dx,0x03d4
 mov al,0x0e
 out dx,al
@@ -24,17 +28,15 @@ mov ah,al
 ;low 8-bit
 mov dx,0x03d4
 mov ax,0x0f
-out dx,ax
+out dx,al
 mov dx,0x03d5
 in al,dx
 ;load the location of cursor into bx
 mov bx,ax
-;
-push ebp
 mov ebp,esp
 mov ecx,[ebp + 36]
-;judge carriage_return   line_feed    backspace   common characters
-;CR 是 0x0d,LF 是 0x0a,BS 是 0x08
+;回车carriage_return:0x0d   换行line_feed:0x0a    
+;后退backspace:0x08
 cmp cl, 0x0d
 jz .is_carriage_ret
 cmp cl, 0x0a
@@ -42,50 +44,76 @@ jz .is_line_feed
 cmp cl, 0x08
 jz .is_backspace
 
-jz .is_common_char
+jmp .is_common_char
 
 
 ;-------------灵活利用bx寄存器做相应的字符处理----------------
 
-.is_line_feed:
 .is_carriage_ret:
-xor dx, dx
-mov ax, bx
-mov si, 80
-div si
-sub bx, dx
-.is_carriage_ret_end:
-add bx, 80
-cmp bx, 2000    
-.is_line_feed_end:
-jl .set_cursor
+mov ax,bx
+mov bl,80
+div bl
+mul bl
+mov bx,ax
+jmp .update_cursor
+
+.is_line_feed:
+;需要考虑roll_screen的情况
+mov dx,bx
+add dx,80
+cmp dx,2000
+jnb .roll_screen
+cmp dx,2000
+add bx,80
+jb .update_cursor
 
 .is_backspace:
-;写法类似common_char，但是最后不需要更改bx的值，也不需要更新cursor位置
-shl bx
-mov [gs:bx],0x20    ;ox20代表０，也可以用空格表示这是一个空字符
-inc bx
-mov [gs:bx],0x07
 dec bx
-shr bx
-;update_cursor 可执行可不执行，因为backspace并没有改变cursor的位置
+shl bx,1
+mov byte [gs:bx],0x20    ;asci(ox20) == 0,代表空字符
+mov byte [gs:bx+1],0x07
+shr bx,1
+jmp .update_cursor
+
 .is_common_char:
-;由bx找到实际显存地址，写入两字节的字符信息后把bx除２转回成索引值index
-shl bx
-mov [gs:bx],cl
+;由bx找到实际显存地址
+;写入两字节的字符信息后把bx除２转回成索引值index
+shl bx,1
+mov byte [gs:bx],cl
+mov byte [gs:bx+1],0x07
+shr bx,1
 inc bx
-mov [gs:bx],0x07
-dec bx  ;其实这里如果不减１也可以，因为下一步右移除２自动去余数
-shr bx
-inc bx
-;bx加一后需要更新cursor位置
 cmp bx,2000
-jl .update_cursor
+jb .update_cursor
+cmp bx,2000 ;再执行cmp的原因是不确定eflag已刷新
+sub bx,80
+jnb .roll_screen
+
+
+.roll_screen:
+;滚屏是(1~24)-->(0~23)并将最后一行清空
+cld
+mov ecx,(24*80)
+;reg已经全部压栈,可修改ds与es
+mov ax,gs
+mov ds,ax
+mov es,ax
+mov esi,80
+mov edi,0
+rep movsw
+inc edi
+mov ecx,80
+.clear_last_line:
+mov dword [gs:edi],0x0720
+add edi,2
+loop .clear_last_line
 
 
 
 .update_cursor:
-;update cursor功能是将新的bx值重新返还给locate_cursor寄存器，以确保光标位置正确
+;update cursor功能是将新的bx值重新返还
+;给locate_cursor寄存器，以确保光标位置正确
+;high 8-bit
 mov dx,0x03d4
 mov al,0x0e
 out dx,al
@@ -99,3 +127,8 @@ out dx,ax
 mov dx,0x03d5
 mov al,bl
 out dx,al
+
+
+.putchar_terminate:
+popad
+ret
