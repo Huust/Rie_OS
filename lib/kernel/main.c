81cf0189cfdf4ea9692244bf3c743f281f8c17ce
Huust <git@github.com:Huust/Rie_OS.git>
2020-04-27 17:37:45 +0800

    add test

--- lib/kernel/main.c
+++ lib/kernel/main.c
@@ -4,18 +4,9 @@ int main(void)
     rie_putc('r');
     rie_putc('i');
     rie_putc('e');
-    rie_putc('\b');
     rie_putc('o');
     rie_putc('s');
-    rie_putc('\n'); 
-    rie_putc('\r'); 
-    rie_putc('k');
-    rie_putc('e');
-    rie_putc('r');
-    rie_putc('n');
-    rie_putc('e');
-    rie_putc('l');
-    
+    rie_puts("\r\nkernel");
     while(1);
     return 0;
 }
