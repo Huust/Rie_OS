44eb9586b12cc23db335db8443827916adf1cd3f
Huust <git@github.com:Huust/Rie_OS.git>
2020-06-07 14:23:56 +0800

    add memory allocation

--- lib/kernel/main.c
+++ lib/kernel/main.c
@@ -6,9 +6,11 @@ int main(void)
 {
     
     rie_puts("rieos kernel\r\n");
-    idt_init();
-    ASSERT(2==3);
+    //idt_init();
+    //ASSERT(2==3);
     //asm volatile ("sti");   //打开全局中断
+    void mem_struct_init(,);
+    void* get_kernel_page();
     while(1);
     return 0;
 }
